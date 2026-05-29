30e45914718aff06ade4311c538200c7d5281385
danielavescia <danivescia@gmail.com>
2026-05-29 13:56:27 -0300

    test: implement shouldReturnEmptyListForNonExistingPage

--- src/test/java/userManagement/GetUsersErrorTest.java
+++ src/test/java/userManagement/GetUsersErrorTest.java
@@ -1,19 +1,30 @@
 package userManagement;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.empty;
 import org.testng.annotations.BeforeClass;
 import config.ConfigManager;
 import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
+import specs.RequestSpecFactory;
+import org.testng.annotations.Test;
 
 public class GetUsersErrorTest {
-    protected static RequestSpecification requestSpec;
-
-    protected static ResponseSpecification responseSpec;
 
     @BeforeClass
     public void setup() {
         RestAssured.baseURI = ConfigManager.get("baseURI");
-
     }
+
+    @Test(description = "Deve retornar lista vazia para página inexistente", groups = "error-users")
+    public void shouldReturnEmptyListForNonExistingPage(){
+        given()
+            .spec(RequestSpecFactory.withValidApiKey())
+            .queryParam("page", 9999)
+        .when()
+            .get("/users")
+        .then()
+            .statusCode(200)
+            .body("data", empty());
+    } 
 }
+
