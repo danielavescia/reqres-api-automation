e48e423fdfdd68c13dbeb2e625b231a0de9d187f
danielavescia <danivescia@gmail.com>
2026-05-29 09:30:17 -0300

    test: implement shouldValidateUsersData test

--- src/test/java/userManagement/GetUsersSucessTest.java
+++ src/test/java/userManagement/GetUsersSucessTest.java
@@ -2,7 +2,7 @@ package userManagement;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import config.ConfigManager;
 import io.restassured.RestAssured;
@@ -18,7 +18,7 @@ public class GetUsersSucessTest {
 
     protected static ResponseSpecification responseSpec;
 
-    @BeforeSuite
+    @BeforeClass
     public void setup() {
         RestAssured.baseURI = ConfigManager.get("baseURI");
 
@@ -29,43 +29,43 @@ public class GetUsersSucessTest {
         responseSpec = new ResponseSpecBuilder()
             .expectStatusCode(200)
             .expectContentType(ContentType.JSON)
-            .build();
-        
+            .build(); 
     }
 
     @Test(description = "Deve retornar usuários da página 2 com sucesso", groups = "users-sucess")
     public void shouldReturnUsersListNotEmpty() {
 
-            given()
-                .spec(requestSpec)
-                .queryParam("page", 1)
-            .when()
-                .get("/users")
-            .then()
-                .spec(responseSpec)
-                .body("page", equalTo(1))
-                .body("data.size()", greaterThan(0));
+        given()
+            .spec(requestSpec)
+            .queryParam("page", 1)
+        .when()
+            .get("/users")
+        .then()
+            .spec(responseSpec)
+            .body("page", equalTo(1))
+            .body("data.size()", greaterThan(0));
     }
 
     @Test(description = "Deve validar que todos os usuários possuem campos obrigatórios", groups = "users-sucess")
     public void shouldValidateAllUsersFields() {
 
-            given()
-                .spec(requestSpec)
-                .queryParam("page", 1)
-            .when()
-                .get("/users")
-            .then()
-                .spec(responseSpec)
-                .body("data[0].id", notNullValue())
-                .body("data[0].email", notNullValue())
-                .body("data[0].first_name", notNullValue())
-                .body("data[0].last_name", notNullValue())
-                .body("data[0].avatar", notNullValue());
+        given()
+            .spec(requestSpec)
+            .queryParam("page", 1)
+        .when()
+            .get("/users")
+        .then()
+            .spec(responseSpec)
+            .body("data[0].id", notNullValue())
+            .body("data[0].email", notNullValue())
+            .body("data[0].first_name", notNullValue())
+            .body("data[0].last_name", notNullValue())
+            .body("data[0].avatar", notNullValue());
     }
 
     @Test(description = "Deve validar o schema da response de GET Usuário", groups = "users-sucess")
     public void shouldValidateUsersSchema(){
+
         given()
             .spec(requestSpec)
             .queryParam("page", 2)
@@ -74,5 +74,23 @@ public class GetUsersSucessTest {
         .then()
             .spec(responseSpec)
             .body(matchesJsonSchemaInClasspath("schemas/users-schema.json"));
+    }
+
+
+    @Test(description = "Deve validar os campos obrigatórios de Usuário", groups = "users-sucess")
+    public void shouldValidateUsersData(){
+        
+        given()
+            .spec(requestSpec)
+            .queryParam("page", 1)
+        .when() 
+            .get("/users")
+        .then()
+            .spec(responseSpec)
+            .body("data.id", everyItem(notNullValue()))
+            .body("data.email", everyItem(notNullValue()))
+            .body("data.first_name", everyItem(notNullValue()))
+            .body("data.last_name", everyItem(notNullValue()))
+            .body("data.avatar", everyItem(notNullValue()));
     }
 }
