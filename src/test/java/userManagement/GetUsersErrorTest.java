5d3b7feb78e0450232f97dc17e07532edb9a03e3
danielavescia <danivescia@gmail.com>
2026-05-29 14:51:04 -0300

    test: implement shouldReturnUnauthorizedWhenApiKeyIsMissing  and shouldReturnForbiddenWhenApiKeyIsInvalid tests

--- src/test/java/userManagement/GetUsersErrorTest.java
+++ src/test/java/userManagement/GetUsersErrorTest.java
@@ -2,6 +2,7 @@ package userManagement;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import org.testng.annotations.BeforeClass;
 import config.ConfigManager;
 import io.restassured.RestAssured;
@@ -25,6 +26,33 @@ public class GetUsersErrorTest {
         .then()
             .statusCode(200)
             .body("data", empty());
-    } 
-}
+    }
 
+    @Test(description = "Deve retornar 401 quando API Key estiver ausente", groups = "error-users")
+    public void shouldReturnUnauthorizedWhenApiKeyIsMissing(){
+        given()
+            .spec(RequestSpecFactory.withoutApiKey())
+            .queryParam("page", 1)
+            .queryParam("_t", System.currentTimeMillis())
+        .when()
+            .get("/users")
+        .then()
+            .statusCode(401)
+            .body("error", equalTo("missing_api_key"))
+            .body("message", equalTo("The x-api-key header is required for this endpoint."));
+    }
+
+    @Test(description = "Deve retornar 403 quando API Key for inválida", groups = "error-users")
+    public void shouldReturnForbiddenWhenApiKeyIsInvalid(){
+        given()
+            .spec(RequestSpecFactory.withInvalidApiKey())
+            .queryParam("page", 1)
+            .queryParam("_t", System.currentTimeMillis())
+        .when()
+            .get("/users")
+        .then()
+            .statusCode(403)
+            .body("error", equalTo("invalid_api_key"))
+            .body("message", equalTo("This API key is not recognized or has been revoked."));
+    }
+}
