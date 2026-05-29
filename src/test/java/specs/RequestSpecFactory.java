988b9ee451b3b32198fa1cc0af91b6b807df8066
danielavescia <danivescia@gmail.com>
2026-05-29 13:20:54 -0300

    test: add RequestSpecFactory for API request specifications

---
+++ src/test/java/specs/RequestSpecFactory.java
@@ -0,0 +1,33 @@
+package specs;
+
+import config.ConfigManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class RequestSpecFactory {
+
+    private RequestSpecFactory() {}
+
+    public static RequestSpecification withValidApiKey() {
+        
+        return new RequestSpecBuilder()
+                .addHeader("x-api-key", ConfigManager.get("api.key"))
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static RequestSpecification withoutApiKey(){
+        return new RequestSpecBuilder() 
+                .setContentType(ContentType.JSON)
+                .build();
+             
+    }
+
+    public static RequestSpecification withInvalidApiKey(){
+        return new RequestSpecBuilder()
+            .addHeader("x-api-key", "invalid-key")
+            .setContentType(ContentType.JSON)
+            .build();
+    }
+}
