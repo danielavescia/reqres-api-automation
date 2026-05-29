664d3456b052df9a913413c852b35a2f37bfb0a7
danielavescia <danivescia@gmail.com>
2026-05-29 14:52:44 -0300

    refactor: remove unnecessary blank lines and clean up imports

--- src/test/java/specs/RequestSpecFactory.java
+++ src/test/java/specs/RequestSpecFactory.java
@@ -4,7 +4,6 @@ import config.ConfigManager;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-
 public class RequestSpecFactory {
 
     private RequestSpecFactory() {}
