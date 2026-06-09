40fdc0568b64f280acc238ad721036575b2029e3
danielavescia <danivescia@gmail.com>
2026-06-09 14:58:23 -0300

    refactor: enhance BaseTest with setupSuite method and improve reset functionality

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,9 @@
 package base;
 
+import java.util.Collections;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import config.ConfigManager;
 import io.qameta.allure.restassured.AllureRestAssured;
@@ -13,14 +15,18 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 @Listeners({AllureTestNg.class})
 public class BaseTest {
 
+    @BeforeSuite
+    public void setupSuite() {
+        RestAssured.baseURI = ConfigManager.get("base.url");
+    }
+
     @BeforeMethod
     public void setup() {
-        RestAssured.baseURI = ConfigManager.get("base.url");
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new AllureRestAssured());
     }
 
     @AfterMethod
     public void reset() {
-        RestAssured.reset();
+           RestAssured.replaceFiltersWith(Collections.emptyList());
     }
 }
