5abdd6a0a7feff73e48283be7f31a8c09ccdd59b
danielavescia <danivescia@gmail.com>
2026-06-09 14:05:16 -0300

    refactor: enhance BaseTest setup with Allure support and reset method

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,16 +1,26 @@
 package base;
 
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import config.ConfigManager;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.qameta.allure.testng.AllureTestNg;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 
+@Listeners({AllureTestNg.class})
 public class BaseTest {
 
-    @BeforeClass
+    @BeforeMethod
     public void setup() {
         RestAssured.baseURI = ConfigManager.get("base.url");
-        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(), new AllureRestAssured());
+    }
+
+    @AfterMethod
+    public void reset() {
+        RestAssured.reset();
     }
 }
