6f31ff76bcce841c4f41865b93e56d6332d1981a
danielavescia <danivescia@gmail.com>
2026-06-10 15:16:59 -0300

    feat: introduce TestConstant for improved test readability and maintainability

---
+++ src/main/java/constant/TestConstant.java
@@ -0,0 +1,17 @@
+package constant;
+
+public class TestConstant {
+
+    public static final int FIRST_PAGE = 1;
+
+    public static final int SECOND_PAGE = 2;
+
+    public static final int PER_PAGE = 6;
+
+    public static final int NON_EXISTENT_PAGE = 999;
+
+    public static final long MAX_TIME_RESPONSE = 2000L;
+
+    private TestConstant () {}
+
+}
